19e2d04249c474250dcd3207fde578dff237a831
Ignat Paskhin <paskhin_92@mail.ru>
2024-05-25 18:17:04 +0300

    Добавляет useEffect, useCallback

--- src/components/dishes/component.jsx
+++ src/components/dishes/component.jsx
@@ -3,9 +3,9 @@ import { Dish } from "../dish/component"
 export function Dishes({menu}) {
   return (
     <ul>
-      {menu.map(({name}) => (
+      {menu.map(({name, price}) => (
         <li>
-          <Dish name={name}/>
+          <Dish name={name} price={price}/>
         </li>
       ))}
     </ul>
